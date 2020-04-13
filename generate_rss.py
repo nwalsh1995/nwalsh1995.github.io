947cb5fb639e7787c1df2355397c240edbd62f65
Nathan Walsh <nwalsh1995@gmail.com>
2020-04-12 22:11:31 -0700

    add rss generator and rss file

---
+++ generate_rss.py
@@ -0,0 +1,21 @@
+from min_rss_gen.generator import start_rss, gen_item
+
+import xml.etree.ElementTree
+from glob import glob
+from pathlib import PurePath
+
+SITE = "https://nwalsh1995.github.io"
+RSS_FILENAME = "rss.xml"
+rss_items = []
+
+for f in glob("**/*.html", recursive=True):
+    path = PurePath(f)
+    print(path)
+    print(path.name)
+    title = path.with_suffix("").name.replace("-", " ").title()
+    rss_items.append(gen_item(title=title, link=f"{SITE}/{str(path)}"))
+
+rss_xml_element = start_rss(title="nwalsh1995.github.io", link="nwalsh1995.github.io", description="A collection of thoughts.", items=rss_items)
+
+with open(RSS_FILENAME, "wb") as f:
+    f.write(xml.etree.ElementTree.tostring(rss_xml_element))
